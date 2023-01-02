683f98e2f1a50958abb8d85a22fd5256310f31c4
angelrush91 <94041779+angelrush91@users.noreply.github.com>
2023-01-02 13:30:38 +0100

    kunde details

--- Unik.Webapp/Pages/Kunde/Details.cshtml.cs
+++ Unik.Webapp/Pages/Kunde/Details.cshtml.cs
@@ -1,35 +1,46 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Unik.WebApp.Infrastructure.Kunde.Contract;
+using Unik.WebApp.Infrastructure.Projekt.Contract;
 
 namespace Unik.WebApp.Pages.Kunde
 {
     public class DetailsModel : PageModel
     {
         private readonly IKundeService _kundeService;
-        public DetailsModel(IKundeService kundeService)
+        private readonly IProjektService _projektService;
+
+        public DetailsModel(IKundeService kundeService, IProjektService projektService)
         {
             _kundeService = kundeService;
+            _projektService = projektService;
         }
 
-        [BindProperty] public List<KundeDetailsViewModel> DetailsViewModel { get; set; } = new();
-        public async Task<IActionResult> OnGet(string? userId)
+        [BindProperty] public KundeDetailsViewModel DetailsViewModel { get; set; } = new();
+        public async Task<IActionResult> OnGet(int id)
         {
-            if (TryValidateModel(string.IsNullOrWhiteSpace(userId))) return NotFound();
+            //if (TryValidateModel(string.IsNullOrWhiteSpace(userId))) return NotFound();
 
-            var model = await _kundeService.GetAll();
+            var model = await _kundeService.Get(id);
 
-            model?.OrderBy(a => a.Id)
-                .ToList()
-                .ForEach(dto => DetailsViewModel.Add(new KundeDetailsViewModel
-                {
-                    Id = dto.Id,
-                    Navn = dto.Navn,
-                    VirksomhedsNavn = dto.VirksomhedNavn,
-                    Email = dto.Email,
-                    Tlf = dto.Tlf,
-                    UserId = dto.UserId
-                }));
+            if (model == null) throw new Exception("der var ikke nogen kunde med det Id");
+
+            var projekt = await _projektService.ProjektGetAll();
+
+            var kundeProjekt = projekt.Where(a => a.kundeID == id).First();
+
+            if (kundeProjekt == null) throw new Exception("kunden har ingen projekter");
+
+
+
+            DetailsViewModel.Id = model.Id;
+            DetailsViewModel.Navn = model.Navn;
+            DetailsViewModel.VirksomhedsNavn = model.VirksomhedNavn;
+            DetailsViewModel.Email = model.Email;
+            DetailsViewModel.Tlf = model.Tlf;
+            DetailsViewModel.UserId = model.UserId;
+            DetailsViewModel.ProjektId = kundeProjekt.Id;
+
 
             return Page();
         }
