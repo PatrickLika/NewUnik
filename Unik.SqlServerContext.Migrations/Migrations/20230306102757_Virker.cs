e7b2ed06a5863858561c0c0ae49d64bba682d7f0
Patrick Lika <pbli38258@edu.ucl.dk>
2023-03-06 11:28:09 +0100

    test

---
+++ Unik.SqlServerContext.Migrations/Migrations/20230306102757_Virker.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unik.SqlServerContext.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class Virker : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Opgave_Medarbejder_MedarbejderId",
+                schema: "opgave",
+                table: "Opgave");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Opgave_MedarbejderId",
+                schema: "opgave",
+                table: "Opgave");
+
+            migrationBuilder.DropColumn(
+                name: "MedarbejderId",
+                schema: "opgave",
+                table: "Opgave");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MedarbejderId",
+                schema: "opgave",
+                table: "Opgave",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Opgave_MedarbejderId",
+                schema: "opgave",
+                table: "Opgave",
+                column: "MedarbejderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Opgave_Medarbejder_MedarbejderId",
+                schema: "opgave",
+                table: "Opgave",
+                column: "MedarbejderId",
+                principalSchema: "medarbejder",
+                principalTable: "Medarbejder",
+                principalColumn: "Id");
+        }
+    }
+}
